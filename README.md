cc8db69d00657ea0f62adfbd84f08f79a9b82cf8
Keith R. Potempa <10837115+keithrpotempa@users.noreply.github.com>
2020-08-10 16:37:07 -0500

    print-to-dom update, readme update

---
+++ README.md
@@ -0,0 +1,15 @@
+# "GOING TO BOSTON" Dice Game
+
+A self-inflicted code challenge written in vanilla HTML and JavaScript: recreate the classic dice game "Going to Boston".
+
+Written 12/13/19, before attending Nashville Software School, based on skills learned with Treehouse courses.
+
+Demo it live [here](https://codesandbox.io/s/dank-sky-4mryc?file=/index.html)!
+
+# How the Game is Played
+
+1. Decide how many rounds to play (x)
+1. gather x dice
+1. Each player rolls x dice on their turn, keeps the highest result
+1. Repeat the previous step with x-1 dice, x-2 dice, etc. until x=0
+1. Highest "kept" score wins!
--- script.js
+++ script.js
@@ -1,48 +1,59 @@
 // Code Challenge: Create the dice game "Going to Boston"
 //http://www.netexl.com/howtoplay/going-to-boston/
 
-const rounds = 3
-let diceToRoll = rounds
+const printhere = document.getElementById("print-here");
+
+function print(text) {
+  printhere.innerHTML += "<p>" + text + "</p>";
+}
+
+function printHeader(text) {
+  printhere.innerHTML += "<h3>" + text + "</h3>";
+}
+
+const rounds = 3;
+let diceToRoll = rounds;
 
 let players = [
-  {name: "Keith"},
-  {name: "Freya"},
-  {name: "Josh"},
-  {name: "Shan"}
-]
+  { name: "Keith" },
+  { name: "Freya" },
+  { name: "Josh" },
+  { name: "Shan" }
+];
 
 // This stuff will change every player, every round
 let rolls = [];
 let roundHighest = 0;
 
 function rollDie() {
-    let min = Math.ceil(1);
-    let max = Math.floor(6);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+  let min = Math.ceil(1);
+  let max = Math.floor(6);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function roundOfRolls() {
   rolls = [];
-  for (let i = 0; i < diceToRoll; i ++) {
+  for (let i = 0; i < diceToRoll; i++) {
     rolls.push(rollDie());
   }
   return rolls;
 }
 
-function determineHighest(){
+function determineHighest() {
   let highest = 0;
-  for (let i = 0; i < rolls.length; i ++) {
+  for (let i = 0; i < rolls.length; i++) {
     if (rolls[i] > highest) {
       highest = rolls[i];
     }
   }
-  return highest
+  return highest;
 }
 
-function roundAnnouncement(player){
-    console.log(player.name + " rolled " + rolls)
-    console.log("Their highest die was a " + roundHighest);
-    console.log("Their current score is: " + player.score)
+function roundAnnouncement(player) {
+  print(player.name + " rolled " + rolls);
+  print("Their highest die was a " + roundHighest);
+  print("Their current score is: " + player.score);
+  print("---");
 }
 
 // Define the game's main function called "rollForRounds"
@@ -52,37 +63,37 @@ function roundAnnouncement(player){
 // Determine and add their highest result to their score property using "determineHighest"
 // Then subtract one from diceToRoll variable each round, to represent rolling one fewer die
 
-function rollForRounds(){
-  for (let round = 1; round <= rounds; round ++) {
-    console.log();
-    console.log("In round " + round + ": " );
-    for (let i = 0; i < players.length; i ++) {
+function rollForRounds() {
+  for (let round = 1; round <= rounds; round++) {
+    print("==========");
+    printHeader("In round " + round + ": ");
+    for (let i = 0; i < players.length; i++) {
       rolls.push(roundOfRolls());
       roundHighest = determineHighest(rolls);
-      players[i].score += roundHighest
+      players[i].score += roundHighest;
       roundAnnouncement(players[i]);
     }
-    diceToRoll --;
+    diceToRoll--;
   }
 }
 
-function openAnnouncement(){
-  console.log('"Going to Boston" Dice Game');
-  console.log("We're playing a game with: ");
-  console.log(players.length + " players and " + rounds + " rounds.");
+function openAnnouncement() {
+  printHeader('"Going to Boston" Dice Game');
+  print("We're playing a game with: ");
+  print(players.length + " players and " + rounds + " rounds.");
 }
 
-function finalScores(){
-  console.log();
-  console.log("Final Scores:");
-  for (let i = 0; i < players.length; i ++){ 
-    console.log(players[i].name + ": " + players[i].score);
+function finalScores() {
+  print("==========");
+  printHeader("Final Scores:");
+  for (let i = 0; i < players.length; i++) {
+    print(players[i].name + ": " + players[i].score);
   }
 }
 
 // In order to start every player off at zero,
 // Run a for loop to give a score property to every player, with 0 as its value
-for (let i = 0; i < players.length; i ++) {
+for (let i = 0; i < players.length; i++) {
   players[i].score = 0;
 }
 
